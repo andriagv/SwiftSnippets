d7adbe2f5ccb4ed3342bf1c6dae7be38cbd8f80a
andria.gv <gvaramiaandria1@gmail.com>
2024-11-06 16:41:34 +0400

    Options for creating new scenes

---
+++ UIKit/SceneDelegate.playground/Contents.swift
@@ -0,0 +1,51 @@
+//  SceneDelegate.swift
+//
+//  Created by Andria Gvaramia on 06.11.24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        //MARK: - სთორიბორდით დაწყება
+        
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        window = UIWindow(windowScene: windowScene)
+//        let storyboard = UIStoryboard(name: "first", bundle: nil)
+//        let initialViewController = storyboard.instantiateViewController(withIdentifier: "first")
+//        window?.rootViewController = initialViewController
+//        window?.makeKeyAndVisible()
+        
+        //MARK: - newVC თი დაწყებდა და მხოლოდ ერთი ფეიჯის გამოყენება
+        
+//        guard let scene = (scene as? UIWindowScene) else { return }
+//        window = UIWindow(windowScene: scene)
+//        let VC = newVC()
+//        window?.rootViewController = VC
+//        window?.makeKeyAndVisible()
+        
+        //MARK: - newVC თი დაწყებდა და შემდეგ შეგვიძია გადასვლა სხვას ცენაზე
+        
+//        guard let scene = (scene as? UIWindowScene) else { return }
+//        window = UIWindow(windowScene: scene)
+//
+//        let VC = FirstVC()
+//        let navigationVC = UINavigationController(rootViewController: VC)
+//
+//        window?.rootViewController = navigationVC
+//        window?.makeKeyAndVisible()
+        
+        //MARK: - newVC თი დაწყებდა და დაბლა გაქ განლაგებული რილაკები ნავიგაციისთვის
+        
+//        guard let scene = (scene as? UIWindowScene) else { return }
+//        window = UIWindow(windowScene: scene)
+//
+//        let VC = TabBarController()
+//        let navVC = UINavigationController(rootViewController: VC)
+//        window?.rootViewController = navVC
+//        window?.makeKeyAndVisible()
+    }
+}
+
